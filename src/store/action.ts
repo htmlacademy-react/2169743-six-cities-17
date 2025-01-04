88a1cf299dc376bcf4f0f6d55f07e90178e4a592
Nikolay <dycsollll@gmail.com>
2025-01-05 00:22:20 +0300

    Правки типизации стора

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,4 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
+import type { TOfferArray } from '@/entities/Offers/types';
 
 export const setCity = createAction<{ city: string }>('city/set');
-export const setOffers = createAction('offers/set');
+export const setOffers = createAction<{ offers: TOfferArray }>('offers/set');
